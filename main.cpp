ee3511eb09ab06eedcc37bd87c2fd9e3b68b7c05
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-03-27 17:51:04 +0200

    main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
-#include <iostream>
+#include "ArrayTemplate.h"
 
 int main() {
 
+    int a[5] = {4, 2, 7, 2, -7};
+    printArray(a, 5);
+
+    int b[20] = {0};
+    randArray(b, 20, -50, 50);
+    printArray(b, 20);
+
+    float f[3]= {3.6, 2.8, 6.12};
+    printArray(f, 3);
 
 
     return 0;
